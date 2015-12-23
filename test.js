9a267cdbe760be80c3487b3ec721153aa7631d8c
justeliminator <tumblrcathleen@gmail.com>
2015-12-23 12:24:23 -0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,24 @@
+var printList = function(){
+    var variable = $('#text').val();
+    var splitText = variable.split(" ");
+    console.log(splitText);
+    var count = 0;
+    for (var i = 0; i < splitText.length; i++) {
+      if (splitText[i] == "just" || splitText[i] == "Just") {
+        count++;        }
+      }
+    console.log(count);
+    document.getElementById("count").innerHTML = count
+}
+
+
+
+function goToHome(){
+    document.getElementById("home").style.display = "";
+    document.getElementById("about").style.display = "none";
+}
+
+function goToAbout(){
+    document.getElementById("home").style.display = "none";
+    document.getElementById("about").style.display = "";
+}
